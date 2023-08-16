a94ea9f59e46dadfbb989bf4b5660e315f16f04f
HamXaLeghari <legharihamxa@gmail.com>
2023-08-16 11:28:55 +0500

    Delete SampleCRUDPractice directory

--- SampleCRUDPractice/Constants/DatabaseConstants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class DatabaseConstants {
-    const DB_HOST = "localhost";
-    const DB_PORT = "5432";
-    const DB_NAME = "student_management_db";
-    const DB_USER = "postgres";
-    const DB_PASSWORD = "12345";
-}
